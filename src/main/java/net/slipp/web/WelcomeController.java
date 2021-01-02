5bbe3ee83edb6ee7b2b494840741159fed452516
wooyongjeong <dndyd1231@gmail.com>
2021-01-02 13:05:29 +0900

    Add html template
    * https://github.com/slipp/web-application-server 에서 clone한 코드로 html template를 추가하였습니다.

--- src/main/java/net/slipp/web/WelcomeController.java
+++ src/main/java/net/slipp/web/WelcomeController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 	@GetMapping("/helloworld")
-	public String welcome(String name, int age, Model model) {
-		System.out.println("name: " + name + ", age: " + age);
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
+	public String welcome(Model model) {
+		model.addAttribute("name", "wooyong");
+		model.addAttribute("value", 10000);
+		model.addAttribute("taxed_value", 30);
+		model.addAttribute("in_ca", true);
 		return "welcome";
 	}
 }
