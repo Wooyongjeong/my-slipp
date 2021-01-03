47cad5eb284c4747ac5a8a68d4e886ab526e5ee3
wooyongjeong <dndyd1231@gmail.com>
2021-01-03 18:43:12 +0900

    개인정보 수정 기능 구현 및 코드 리팩토링
    * html에서 반복되는 부분을 /template/include 폴더에 따로 빼어 mustache을 이용하여 정리하였습니다.
    
    * 불필요한 controller와 이전에 구현했던 html 등을 삭제하였습니다.
    
    * 개인정보 수정 기능 구현하였습니다.

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
@@ -15,6 +17,11 @@ import net.slipp.domain.UserRepository;
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
 	
 	@PostMapping("")
 	public String create(User user) {
@@ -27,5 +34,20 @@ public class UserController {
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id).get();
+		model.addAttribute("user", user);
+		return "/user/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findById(id).get();
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
