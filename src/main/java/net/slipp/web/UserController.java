f91ba0b665a68235e8a55648eda0fabdbb8fcdb7
wooyongjeong <dndyd1231@gmail.com>
2021-01-04 14:10:57 +0900

    로그인 기능 구현 완료 및 footer 반복 제거

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,5 +1,9 @@
 package net.slipp.web;
 
+import java.awt.desktop.UserSessionEvent;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +21,29 @@ import net.slipp.domain.UserRepository;
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping("loginForm")
+	public String loginForm() {
+		return "user/login";
+	}
+	
+	@PostMapping("login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		
+		if(user == null) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		if(!password.equals(user.getPassword())) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		System.out.println("Login Success!");
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+	}
 	
 	@GetMapping("form")
 	public String form() {
