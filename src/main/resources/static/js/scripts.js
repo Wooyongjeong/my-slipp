c8beb62d0ff956d732ab52d2fade6474884fca46
wooyongjeong <dndyd1231@gmail.com>
2021-01-05 15:09:07 +0900

    AJAX를 활용한 답변 추가 기능 구현
    * @RestController 어노테이션 이용하여 json api 구현하였습니다.
    
    * Dto 클래스에 @JsonProperty, @JsonIgnore 어노테이션 이용하여 getter 메서드가 없는 필드도 json으로 전달되도록 구현하였습니다.
    
    * scripts.js에서 jQuery를 이용하여 ajax 구현하였고, template 이용하여 동적으로 답변 추가기능 구현하였습니다.

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,43 @@
+$(".answer-write input[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+	console.log("clicked");
+	e.preventDefault();
+
+	var queryString = $(".answer-write").serialize();
+	console.log("query: " + queryString);
+
+	var url = $(".answer-write").attr("action");
+	console.log(url);
+
+	$.ajax({
+		type: 'post',
+		url: url,
+		data: queryString,
+		dataType: 'json',
+		error: onError,
+		success: onSuccess
+	});
+}
+
+function onError() {
+
+}
+
+function onSuccess(data, status) {
+	console.log(data);
+	var answerTemplate = $("#answerTemplate").html();
+	var template = answerTemplate.format(data.writer.userId, data.formattedCreateDate, data.contents, data.id, data.id);
+	$(".qna-comment-slipp-articles").prepend(template);
+	$(".answer-write textarea").val('');
+}
+
 String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
+	var args = arguments;
+	return this.replace(/{(\d+)}/g, function(match, number) {
+		return typeof args[number] != 'undefined'
+			? args[number]
+			: match
+			;
+	});
 };
