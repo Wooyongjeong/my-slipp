e5f2dbbc432e61a4f9f5bfecc1688981b710a232
wooyongjeong <dndyd1231@gmail.com>
2021-01-05 16:52:35 +0900

    질문 목록(메인 페이지)에서 답변 수 보여주기 기능 추가
    * 질문 클래스에 countOfAnswer 필드 추가하여 Question 테이블에서 답변 개수를 저장하도록 추가하였습니다.
    
    * 질문에 대한 답변이 추가되거나 삭제될 때 질문 클래스에서 addAnswer(), deleteAnswer() 메서드를 통해 countOfAnswer 필드의 값을 변경토록 하여 쿼리를 날리지 않고 답변 개수를 변경하도록 구현하였습니다.

--- src/main/java/net/slipp/web/ApiAnswerController.java
+++ src/main/java/net/slipp/web/ApiAnswerController.java
@@ -33,6 +33,7 @@ public class ApiAnswerController {
 		User loginUser = HttpSessionUtils.getUserFromSession(session);
 		Question question = questionRepository.findById(questionId).get();
 		Answer answer = new Answer(loginUser, question, contents);
+		question.addAnswer();
 		return answerRepository.save(answer);
 	}
 	
@@ -47,6 +48,9 @@ public class ApiAnswerController {
 			return Result.fail("자신의 글만 삭제할 수 있습니다.");
 		}
 		answerRepository.deleteById(id);
+		Question question = questionRepository.findById(questionId).get();
+		question.deleteAnswer();
+		questionRepository.save(question);
 		return Result.ok();
 	}
 }
